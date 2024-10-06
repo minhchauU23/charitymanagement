63d294c4162d3055637ffecbea791e1c2ddbd8b9
minhchauU23 <chauminhtran2@gmail.com>
2024-10-06 15:18:54 +0700

    setup: profiles

---
+++ src/main/java/com/dev/be/charitymanagement/CharitymanagementApplication.java
@@ -0,0 +1,13 @@
+package com.dev.be.charitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharitymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharitymanagementApplication.class, args);
+	}
+
+}
